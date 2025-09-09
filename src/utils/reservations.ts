d677587873c7e3d6f0b4e86da70eab3a5bb11343
Burhanuddin Salim <burhan.salman@gmail.com>
2025-09-09 16:31:38 +0400

    fix: add startsWith check to reservations

--- src/utils/reservations.ts
+++ src/utils/reservations.ts
@@ -21,7 +21,7 @@ export async function getReservations(): Promise<SAPReservationDocument[]> {
       `CreationDateTime gt ${lastSyncISO}`,
       `LastChangeDateTime gt ${lastSyncISO}`,
     ],
-    ["IssuingOrReceivingStorageLoc eq 'CS01'"],
+    ["startswith(IssuingOrReceivingStorageLoc,'CS')"],
   ];
 
   const baseUrl = `${process.env.SAP_API_URL}/sap/opu/odata4/sap/api_reservation_document/srvd_a2x/sap/apireservationdocument/0001`;
