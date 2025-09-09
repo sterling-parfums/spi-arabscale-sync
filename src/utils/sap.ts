c329802ddc3951df661f57a47dcda284bee691da
Burhanuddin Salim <burhan.salman@gmail.com>
2025-09-09 16:41:34 +0400

    chore: remove sap link log

--- src/utils/sap.ts
+++ src/utils/sap.ts
@@ -2,8 +2,6 @@ export async function getSAP(url: string) {
   const username = process.env.SAP_API_USERNAME;
   const password = process.env.SAP_API_PASSWORD;
 
-  console.log("🔗 Fetching SAP URL:", url);
-
   return fetch(url, {
     method: "GET",
     headers: {
