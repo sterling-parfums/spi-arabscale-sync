106064d728e0fb38b021c135d18753859bc0efad
Burhanuddin Salim <burhan.salman@gmail.com>
2025-09-17 16:05:24 +0400

    feat: add debug logs

--- src/utils/sap.ts
+++ src/utils/sap.ts
@@ -2,6 +2,10 @@ export async function getSAP(url: string) {
   const username = process.env.SAP_API_USERNAME;
   const password = process.env.SAP_API_PASSWORD;
 
+  if (process.env.DEBUG === "1") {
+    console.log(`🔍 Fetching SAP URL: ${url}`);
+  }
+
   return fetch(url, {
     method: "GET",
     headers: {
